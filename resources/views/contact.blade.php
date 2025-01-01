ec320afae8d38e530c39dd80047fdfdd12b7a393
Aditya Wirayudha <83379121+Adityawir0410@users.noreply.github.com>
2025-01-01 22:31:01 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+</head>
+
+<body>
+    <h1>ini contact</h1>
+    <ul>
+        <li>
+            <a href="https://instagram.com/Adityawirz">instagram</a>
+        </li>
+        <li>
+            <a href="https://instagram.com/Adityawirz">Twitter</a>
+        </li>
+        <li>
+            <a href="https://instagram.com/Adityawirz">Youtube</a>
+        </li>
+    </ul>
+</body>
+
+</html>
